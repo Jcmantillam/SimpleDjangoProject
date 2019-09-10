d6c27604498c49fc451535cc7eea78434d3b2343
Johan camilo Mantilla <jcmantillam@unal.edu.co>
2019-09-10 14:19:35 -0500

    Allowed AWS server

--- code_web/static/serviceworker.js
+++ code_web/static/serviceworker.js
@@ -4,7 +4,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/home'
+        '/home',
+        '/about',
       ]);
     })
   );
@@ -15,6 +16,21 @@ self.addEventListener('fetch', function(event) {
     if (requestUrl.origin === location.origin) {
       if ((requestUrl.pathname === '/')) {
         event.respondWith(caches.match('/home'));
+        return;
+      }
+    }
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+      if ((requestUrl.pathname === '/')) {
+        event.respondWith(caches.match('/about'));
         return;
       }
     }
