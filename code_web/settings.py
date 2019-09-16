888bb54d6c41fa7f8b350353f70b3a1ac6ccd99f
Johan Mantilla <johanmantilla@JohanMantilla.local>
2019-09-16 14:41:32 -0500

    mejora de front

--- code_web/settings.py
+++ code_web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-'''
+#'''
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 #ServiceWorker to PWA
